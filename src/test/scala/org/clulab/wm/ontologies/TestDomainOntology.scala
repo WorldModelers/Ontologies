8e2bf825442159030027771dba1d2ff87be92995
Keith Alcock <github@keithalcock.com>
2019-10-08 16:19:32 -0700

    Add check for duplicate leaves

--- src/test/scala/org/clulab/wm/ontologies/TestDomainOntology.scala
+++ src/test/scala/org/clulab/wm/ontologies/TestDomainOntology.scala
@@ -9,7 +9,7 @@ import scala.collection.mutable
 
 class TestDomainOntology extends FlatSpec with Matchers {
 
-  def hasDuplicates(network: EidosNetwork): Boolean = {
+  def hasDuplicatePaths(network: EidosNetwork): Boolean = {
     val visitor = new network.HierarchicalGraphVisitor()
     var paths = List.empty[Seq[String]]
     var path = mutable.Seq.empty[String]
@@ -22,10 +22,39 @@ class TestDomainOntology extends FlatSpec with Matchers {
         println(newPath)
         if (paths.contains(newPath)) {
           duplicate = true
-          println(s"Duplicate: $newPath")
+          println(s"Duplicate path: $newPath")
         }
         else
           paths = newPath :: paths
+      }
+      else
+        if (depth < path.size)
+          path(depth) = node.name
+        else
+          path = path :+ node.name
+      true
+    }
+    duplicate
+  }
+
+  def hasDuplicateLeaves(network: EidosNetwork): Boolean = {
+    val visitor = new network.HierarchicalGraphVisitor()
+    var leaves = List.empty[String]
+    var path = mutable.Seq.empty[String]
+    var duplicate = false
+
+    visitor.foreachNode { (node: EidosNode, depth: Int) =>
+      if (network.isLeaf(node)) {
+        val newPath = path.slice(0, depth) :+ node.name
+        val newLeaf = node.name
+
+        println(newPath)
+        if (leaves.contains(newLeaf)) {
+          duplicate = true
+          println(s"Duplicate leaf: $newPath")
+        }
+        else
+          leaves = newLeaf :: leaves
       }
       else
         if (depth < path.size)
@@ -65,18 +94,26 @@ class TestDomainOntology extends FlatSpec with Matchers {
     spaces
   }
 
-  behavior of "ontologies"
-
   def test(path: String): Unit = {
-    it should "load and not have duplicates or spaces in " + path in {
       val network = new EidosNetwork()
       val reader = new EidosReader(network)
 
+    behavior of "ontology in " + path
+
+    it should "load" in {
       // This is the load part which will fail on exception.
       reader.readFromFile(path)
-      println("Checking for duplicates")
-      hasDuplicates(network) should be (false)
-      println("Checking for spaces")
+    }
+
+    it should "not have duplicate paths" in {
+      hasDuplicatePaths(network) should be (false)
+    }
+
+    it should "not have duplicate leaves" in {
+      hasDuplicateLeaves(network) should be (false)
+    }
+
+    it should "not have spaces" in {
       hasSpaces(network) should be (false)
     }
   }
