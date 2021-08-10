90fe546e50b63dccba964ff08a0aaf5a64ffb711
zupon <zupon@email.arizona.edu>
2021-08-10 14:36:45 -0700

    tidies up merge_yamls

--- scripts/merge_yamls.py
+++ scripts/merge_yamls.py
@@ -5,15 +5,13 @@ import hiyapyco
 
 def main():
     if len(sys.argv) != 4:
-        print("usage: python strip_metadata.py [yaml1] [yaml1] [output_file]")
+        print("usage: python merge_yamls.py [path_to_input_file_1] [path_to_input_file_2] [output_file]")
     else:
         merged_yaml = hiyapyco.load(sys.argv[1], sys.argv[2], method=hiyapyco.METHOD_MERGE)
-        error = False
-        mergedLines = hiyapyco.dump(merged_yaml)
-        if not error:
-            with io.open(sys.argv[3], "w") as wm:
-                wm.write(mergedLines)
-            print("Successfully merged yaml files!")
+        merged = hiyapyco.dump(merged_yaml)
+        with io.open(sys.argv[3], "w") as wm:
+            wm.write(merged)
+        print("Successfully merged yaml files!")
 
 
 if __name__ == '__main__':
