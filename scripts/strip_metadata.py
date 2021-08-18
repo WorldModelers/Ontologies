1d670714ac3416367baab1869d2082ddbcf8fca0
zupon <zupon@email.arizona.edu>
2021-08-18 15:40:31 -0700

    updates strip_metadata in prep for pytest

--- scripts/strip_metadata.py
+++ scripts/strip_metadata.py
@@ -10,29 +10,31 @@ except ImportError:
     from yaml import Loader, Dumper
 
 
+def strip_metadata(input_text):
+    rBranchingNode = re.compile(r'\w*-.*:')
+    rOntoNode = re.compile(r'\w*(OntologyNode:)\w*')
+    rName = re.compile(r'\w*(name:)\w*')
+    wmLines = []
+    for line in input_text.split("\n"):
+        if len(rBranchingNode.findall(line)) == 1 and len(rOntoNode.findall(line)) == 0:
+            wmLines.append(line)
+        if len(rName.findall(line)) == 1:
+            line = re.sub(r'( ){2}name: ', '- ', line)
+            wmLines.append(line)
+    return wmLines
+
+
 def main():
     if len(sys.argv) != 3:
         print("usage: python strip_metadata.py [path_to_metadata_yaml] [output_file]")
     else:
-        rBranchingNode = re.compile(r'\w*-.*:')
-        rOntoNode = re.compile(r'\w*(OntologyNode:)\w*')
-        rName = re.compile(r'\w*(name:)\w*')
-        wmLines = []
-        error = False
-        with io.open(sys.argv[1], "r") as ua:
+        with open(sys.argv[1], "r") as ua:
             ua_yaml = yaml.load(ua, Loader=Loader)
-            yaml_arranged = dump(ua_yaml, Dumper=Dumper)  # type=str
-            for line in yaml_arranged.split("\n"):
-                if len(rBranchingNode.findall(line)) == 1 and len(rOntoNode.findall(line)) == 0:
-                    wmLines.append(line)
-                if len(rName.findall(line)) == 1:
-                    line = re.sub(r'( ){2}name: ', '- ', line)
-                    wmLines.append(line)
-        if not error:
-            with io.open(sys.argv[2], "w") as wm:
-                for line in wmLines:
-                    wm.write(line + "\n")
-            print("Successfully converted to stripped (human-diffable) format!")
+            input_text = dump(ua_yaml, Dumper=Dumper)  # type=str
+        with open(sys.argv[2], "w") as wm:
+            for line in strip_metadata(input_text):
+                wm.write(line + "\n")
+        print("Successfully converted to stripped (human-diffable) format!")
 
 
 if __name__ == '__main__':
