94ea85f7c010633555a97930d20f6265bf553690
Keith Alcock <github@keithalcock.com>
2021-09-27 13:14:17 -0700

    Take care of some of the comments

--- extras/src/main/scala/org/clulab/wm/ontologies/apps/Converter.scala
+++ extras/src/main/scala/org/clulab/wm/ontologies/apps/Converter.scala
@@ -3,6 +3,7 @@ package org.clulab.wm.ontologies.apps
 import org.clulab.linnaeus.model.graph.eidos.EidosNetwork
 import org.clulab.linnaeus.model.graph.eidos.EidosNode
 import org.clulab.linnaeus.model.io.eidos.EidosReader
+import org.clulab.wm.eidos.utils.FileUtils
 import org.yaml.snakeyaml.DumperOptions
 import org.yaml.snakeyaml.DumperOptions.FlowStyle
 import org.yaml.snakeyaml.Yaml
@@ -17,12 +18,22 @@ object Converter extends App {
 //  val inputFile = "./CompositionalOntology_metadata.yml"
   val inputFile = "./wm_flat_metadata.yml"
   val outputFile = "./Revised-Ontology_metadata.yml"
-  val network = {
+  val (headerText, network) = {
     val network = new EidosNetwork()
     val reader = new EidosReader(network)
+    val text = FileUtils.getTextFromFile(inputFile)
+    val lines = text.split('\n')
+    val headers = lines.takeWhile(_.startsWith("#"))
+    val trailers = lines
+        .drop(headers.length)
+        .filter(!_.trim.startsWith("#")) // Do not accept complete comment lines.
+        // The lines starting a list can't use this trick.  Comments will remain.
+        .map(line => if (line.trim.startsWith("-")) line else line.replace('#', '@'))
 
-    reader.readFromFile(inputFile)
-    network
+    val trailerText = trailers.mkString("\n")
+
+    reader.readFromString(trailerText)
+    (headers.mkString("", "\n", "\n"), network)
   }
   val eidosToLocalMap = new JIdentityHashMap[EidosNode, LocalNode]()
 
@@ -46,7 +57,7 @@ object Converter extends App {
   val localRoot = eidosToLocalMap.get(network.getRootNode.get)
   // See https://stackoverflow.com/questions/57728245/how-can-i-control-yaml-indentation-using-snakeyaml-during-dumping
   val dumperOptions = new DumperOptions()
-  dumperOptions.setWidth(140)
+  dumperOptions.setWidth(300)
   dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK)
   dumperOptions.setIndent(2)
   dumperOptions.setIndicatorIndent(2)
@@ -65,9 +76,9 @@ object Converter extends App {
       }
       .toMap
   val newLines = lines.zip(indents).map { case (line, indent) =>
-    " " * indentMap(indent) + line.substring(indent)
+    " " * indentMap(indent) + line.substring(indent).replace('@', '#')
   }
-  val newYaml = newLines.mkString("\n")
+  val newYaml = headerText + newLines.mkString("\n")
 
   println(newYaml)
 }
@@ -83,12 +94,13 @@ class LocalNode(eidosNode: EidosNode) extends JLinkedHashMap[String, Any] {
   }
   if (eidosNode.descriptions.nonEmpty)
     node.put("descriptions", eidosNode.descriptions.asJava)
+  // For flat, patterns come before examples and name.  Otherwise, put it after examples.
+  if (eidosNode.patterns.nonEmpty)
+    node.put("pattern", eidosNode.patterns.asJava) // change back to patterns
   if (eidosNode.examples.nonEmpty)
     node.put("examples", eidosNode.examples.asJava)
   // TODO: Move back up to top
   node.put("name", eidosNode.name) // required
-  if (eidosNode.patterns.nonEmpty)
-    node.put("pattern", eidosNode.patterns.asJava) // change back to patterns
   // opposite: [String]
   eidosNode.oppositeOpt.foreach { opposite =>
     node.put("opposite", opposite)
