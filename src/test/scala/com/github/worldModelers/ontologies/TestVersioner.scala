b82a881b788b29626c30b25660630e93ed3e3dbd
Keith Alcock <github@keithalcock.com>
2021-10-18 14:31:56 -0700

    Split up Versioner a little

--- src/test/scala/com/github/worldModelers/ontologies/TestVersioner.scala
+++ src/test/scala/com/github/worldModelers/ontologies/TestVersioner.scala
@@ -1,36 +1,13 @@
 package com.github.worldModelers.ontologies
 
-import org.clulab.wm.eidos.utils.Closer.AutoCloser
+import org.clulab.wm.eidos.utils.Versioner
 import org.scalatest._
-
-import java.util.Properties
-import java.time.ZonedDateTime
 
 class TestVersioner extends FlatSpec with Matchers {
   // This has to be coordinated with the build file.
   val resourceBase = "/org/clulab/wm/eidos/english/ontologies/"
 
-  protected def extensionless(filename: String): String =
-    if (filename.contains('.')) filename.substring(0, filename.lastIndexOf('.'))
-    else filename
-
   behavior of "Versions"
-
-  def getVersionAndDate(filename: String): (String, ZonedDateTime) = {
-    val path = resourceBase + extensionless(filename) + ".properties"
-    val properties = this.getClass.getResourceAsStream(path).autoClose { inputStream =>
-      val properties = new Properties
-
-      properties.load(inputStream)
-      properties
-    }
-    val version = properties.getProperty("hash")
-    val date = properties.getProperty("date")
-    val zonedDateTime = ZonedDateTime.parse(date)
-
-    (version, zonedDateTime)
-  }
-
 
   it should "version" in {
     Seq(
@@ -38,7 +15,7 @@ class TestVersioner extends FlatSpec with Matchers {
       "wm_flat_metadata.yml",
       "CompositionalOntology_metadata.yml"
     ).foreach { filename =>
-      val (version, zonedDateTime) = getVersionAndDate(filename)
+      val (version, zonedDateTime) = Versioner.getVersionAndDate(resourceBase, filename)
 
       println(s"$filename")
       println(s"  version: $version")
